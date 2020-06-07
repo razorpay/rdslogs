c7a42df86ccf3d0263ecf6ccf400ad64d74d07a5
ankitjain28may <ankitjain28may77@gmail.com>
2020-06-07 14:22:52 +0530

    Added File as a backend

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -4,7 +4,9 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"path"
 	"strings"
 	"time"
 
@@ -133,4 +135,34 @@ type STDOUTPublisher struct {
 
 func (s *STDOUTPublisher) Write(line string) {
 	io.WriteString(os.Stdout, line)
+}
+
+// FILEPublisher implements Publisher and save to file
+type FILEPublisher struct {
+	FileName string
+	Path     string
+	Suffix   *string
+}
+
+func (s *FILEPublisher) Write(line string) {
+	suffix := ""
+	if *s.Suffix != "" {
+		splitMarker := strings.Split(*s.Suffix, ":")
+		suffix = "." + splitMarker[0]
+	}
+
+	filename := s.Path + suffix
+	if err := os.MkdirAll(path.Dir(filename), os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := f.Write([]byte(line)); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
